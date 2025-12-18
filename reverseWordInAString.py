c8f934f60d222c7411dced41dd2c8267957865a1
Sanidhya Vijay <sanidhyavijay18@gmail.com>
2025-12-18 10:10:27 +0530

    Added leetcode link

--- reverseWordInAString.py
+++ reverseWordInAString.py
@@ -1,4 +1,4 @@
-
+# https://leetcode.com/problems/reverse-words-in-a-string/submissions/1857675855/?envType=study-plan-v2&envId=leetcode-75
 def reverseWords(self, s: str) -> str:
         sequence = s.split(" ")
         sequence_without_space = [wds for wds in sequence if len(wds) != 0]
